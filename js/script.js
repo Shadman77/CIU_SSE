7564268847fbf5eb18f4215110aecb5b203f665e
Shadman Saif Anonno <38778826+Shadman77@users.noreply.github.com>
2019-12-15 11:45:35 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/**Marquee */
+/**All the class need is to be a block element and overflow = hidden */
+$('.latest-news-marquee').marquee({
+    //speed in milliseconds of the marquee
+    duration: 10000,
+    //gap in pixels between the tickers
+    gap: 50,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: false,
+    pauseOnHover: true,
+    allowCss3Support: true
+});
+
+/**Main Carousel */
+$(document).ready(function() {
+    $('.main-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+/**Event Carousel */
+$(document).ready(function() {
+    $('.event-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear'
+    })
+})
