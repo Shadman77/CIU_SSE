355a60d1405910a253ce687928ae3cde6fe713ad
Shadman Saif Anonno <38778826+Shadman77@users.noreply.github.com>
2019-12-21 13:13:52 +0600

    Main Carousel update

--- js/index-page.js
+++ js/index-page.js
@@ -14,20 +14,6 @@ $('.notice').marquee({
     pauseOnHover: true,
     allowCss3Support: true
 });
-/**Main Carousel */
-$(document).ready(function() {
-    $('.main-carousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        fade: true,
-        cssEase: 'linear'
-    });
-});
 
 /**Event Carousel */
 $(document).ready(function() {
